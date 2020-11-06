ee0fa30ef0b9ea4954cb665bfc4d268360fb14f5
Gustavo Velasquez <gvelasquez@consultoriaglobal.com.ar>
2020-11-06 13:10:59 -0300

    Updates

---
+++ precache-manifest.731a166075556bc154cf54f3446f328d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e559a2d6129875dd7ba0c57d7a1d2f37",
+    "url": "/news-feeds/index.html"
+  },
+  {
+    "revision": "a8f6f54f5aa2b54d4a0f",
+    "url": "/news-feeds/static/css/main.690b4a9b.chunk.css"
+  },
+  {
+    "revision": "7a780b48d3d0f7e764d7",
+    "url": "/news-feeds/static/js/2.730b44b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-feeds/static/js/2.730b44b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f6f54f5aa2b54d4a0f",
+    "url": "/news-feeds/static/js/main.216e1622.chunk.js"
+  },
+  {
+    "revision": "be5254fc7f78e08e6ce0",
+    "url": "/news-feeds/static/js/runtime-main.b101b7c0.js"
+  },
+  {
+    "revision": "9bf4b416266b853cee24bfa92c70194c",
+    "url": "/news-feeds/static/media/cancel.9bf4b416.svg"
+  },
+  {
+    "revision": "700dba7024b41484270b6d1dc85d2f7d",
+    "url": "/news-feeds/static/media/facebook.700dba70.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/news-feeds/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "337899dfc3c526d0c0b63d8d35a11dad",
+    "url": "/news-feeds/static/media/loading.337899df.svg"
+  },
+  {
+    "revision": "b3826011a627311cfe936140b268d836",
+    "url": "/news-feeds/static/media/news-room.b3826011.png"
+  },
+  {
+    "revision": "093cce66643218cf3b70a2fe80845585",
+    "url": "/news-feeds/static/media/next.093cce66.svg"
+  },
+  {
+    "revision": "3ac47e479b8f15dabbbde6679baf1728",
+    "url": "/news-feeds/static/media/nextBlocked.3ac47e47.svg"
+  },
+  {
+    "revision": "248e935901d3a22d84ca75179ec57cd2",
+    "url": "/news-feeds/static/media/previous.248e9359.svg"
+  },
+  {
+    "revision": "1619d2cbd7abbda857e6d46c64d110da",
+    "url": "/news-feeds/static/media/previousBlocked.1619d2cb.svg"
+  },
+  {
+    "revision": "7484088f716a475b03cd7f4af8b5a3ce",
+    "url": "/news-feeds/static/media/search.7484088f.svg"
+  },
+  {
+    "revision": "3ec51afbe422ce8c384b7ad1024d715a",
+    "url": "/news-feeds/static/media/searchBlocked.3ec51afb.svg"
+  },
+  {
+    "revision": "d94af8cb7745547e967ee3de8697f602",
+    "url": "/news-feeds/static/media/technical_difficulties.d94af8cb.jpg"
+  },
+  {
+    "revision": "a57ea219084451288a1f02aabba16f1b",
+    "url": "/news-feeds/static/media/twitter.a57ea219.svg"
+  }
+]);
